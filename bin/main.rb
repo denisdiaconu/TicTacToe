794e913b3b284f5da2aa7a726a1dd57f868f7a3c
nganifaith <nganifaith@gmail.com>
2021-02-15 16:28:20 +0100

    add board to main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,16 @@
 require_relative '../lib/game_logic'
 require_relative '../lib/user_interface'
 
-puts 'Hello World!'
+def board(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "__________"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "__________"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  puts "__________"
+
+end
+
+board_val = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+board(board_val)
