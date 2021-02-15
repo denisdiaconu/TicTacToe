af770cb1993111c6bd04bd97b41f6894845bc9d5
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-15 17:02:56 +0100

    create players

--- bin/main.rb
+++ bin/main.rb
@@ -3,26 +3,31 @@ require_relative '../lib/user_interface'
 
 def board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts "----------"
+  puts '----------'
   puts "#{board[3]} | #{board[4]} | #{board[5]}"
-  puts "----------"
+  puts '----------'
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
-  puts "----------"
-
+  puts '----------'
 end
 
 board_val = %w[1 2 3 4 5 6 7 8 9]
 
 board(board_val)
 
-def create_player
-    puts "Enter your name player one:"
-    player_one = gets.chomp
-    puts "Enter your symbol #{player_one}:"
-    player_one_symbol = gets.chomp
-
-end
-
 class Players
-    def
+  def create_player
+    puts 'Enter your name player:'
+    player = gets.chomp
+    puts "Enter your symbol #{player}:"
+    player_symbol = gets.chomp
+
+    return [player, player_symbol]
+  end
 end
+
+player_one = Players.new
+player = player_one.create_player
+
+player2 = player_one.create_player
+
+puts player,player2
