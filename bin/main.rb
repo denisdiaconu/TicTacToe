f5e8dd1495372e180659ea15b1107be2ffabcf80
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-15 16:38:45 +0100

    user interface player creation

--- bin/main.rb
+++ bin/main.rb
@@ -3,14 +3,26 @@ require_relative '../lib/user_interface'
 
 def board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts "__________"
+  puts "----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]}"
-  puts "__________"
+  puts "----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
-  puts "__________"
+  puts "----------"
 
 end
 
-board_val = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+board_val = %w[1 2 3 4 5 6 7 8 9]
 
 board(board_val)
+
+def create_player
+    puts "Enter your name player one:"
+    player_one = gets.chomp
+    puts "Enter your symbol #{player_one}:"
+    player_one_symbol = gets.chomp
+
+end
+
+class Players
+    def
+end
