7513eb24775aca7f8abf0e8902112c4a82a78f42
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-16 12:08:31 +0100

    game logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,1 +1,28 @@
+class GameLogic
+  attr_accessor :player1, :player2
 
+  def initialize
+    puts 'Welcome to the Tic Tac Toe game!'
+    @player1 = create
+    @player2
+  end
+
+  def create_player(num)
+    puts "Enter your name player #{num}:"
+    name = gets.chomp
+    puts "Enter your symbol #{name}"
+    symbol = gets.chomp
+  end
+
+  def get_symbol(sym_dup)
+    loop do
+      puts 'What 1 letter/character would you like to be your game marker?'
+      puts "It can not be#{sym_dup} if sym_dup"
+      @input = gets.chomp
+      break if @input.match?(/^.$/) && @input != sym_dup
+
+      puts 'Sorry, that is an invalid answer. Please, try again'
+    end
+    @input
+  end
+end
