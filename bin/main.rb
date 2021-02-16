6e93f641cac6dbb648e64e8e7c4bae2b58024c75
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-16 14:13:43 +0100

    working on game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,56 +1,7 @@
-#!/usr/bin/env ruby
 require_relative '../lib/game_logic'
 require_relative '../lib/user_interface'
 
-def board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts '----------'
-  puts "#{board[3]} | #{board[4]} | #{board[5]}"
-  puts '----------'
-  puts "#{board[6]} | #{board[7]} | #{board[8]}"
-  puts '----------'
-end
 
-board_val = %w[1 2 3 4 5 6 7 8 9]
-
-board(board_val)
-
-def create_player
-  puts 'Enter your name player:'
-  player = gets.chomp
-  puts 'Symbols: X or O'
-  puts "Enter your symbol #{player}:"
-  player_symbol = gets.chomp
-  true_sym = %w[X O]
-  until true_sym.include?(player_symbol.upcase)
-    puts 'Wrong symbol, enter the symbol again!'
-    puts "Enter your symbol #{player}:"
-    player_symbol = gets.chomp
-  end
-  player_symbol = player_symbol.upcase
-
-  Player.new(player, player_symbol)
-end
-
-class Player
-  attr_accessor :name, :symbol
-
-  def initialize(name, symbol)
-    @name = name
-    @symbol = symbol
-  end
-
-  def show_player
-    puts @name
-    puts @symbol
-  end
-end
-
-player1 = create_player
-player2 = create_player
-
-puts "#{player1.name} has the symbol #{player1.symbol}!"
-puts "#{player2.name} has the symbol #{player2.symbol}!"
 # puts board_val
 
 # implement the play
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,10 +1,46 @@
 class GameLogic
-    def initialize(player_one, player_two)
-        @player_one = player_one
-        @player_two = player_two
+  attr_accessor :player1, :player2
+  def initialize
+    puts 'Welcome to the Tic Tac Toe game!'
+    @player1 = create_player(1)
+    @player2 = create_player(2, player1.symbol)
+    play_game
+  end
+  def create_player(num, symbol = nil)
+    puts "Enter your name player #{num}:"
+    name = gets.chomp
+    puts "Enter your symbol #{name}"
+    symbol = get_symbol(symbol)
+    player = Player.new(name, symbol)
+  end
+  def get_symbol(sym_dup)
+    loop do
+      puts 'What 1 letter/character would you like to be your game marker?'
+      puts "It can not be #{sym_dup} " if sym_dup
+      @input = gets.chomp.upcase
+      break if @input.match?(/^.$/) && @input != sym_dup
+      puts 'Sorry, that is an invalid answer. Please, try again'
     end
-
+    @input
+  end
+  def play_game
+    @board = Board.new
+    @board.print_board
+  end
+  def is_over
+    @board.complete_line?(player1.symbol, player2.symbol)
+  end
+  def player_turn
+  end
+end
+class Player
+    attr_accessor :name, :symbol
+    def initialize(name, symbol)
+        @name = name
+        @symbol = symbol
+    end
+end
+game = GameLogic.new
     def win(board)
-        
     end
 end
