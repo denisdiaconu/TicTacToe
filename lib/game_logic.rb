ed90996eabbaf31a0bd478fcaedef0fd5dd7f746
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-16 10:09:44 +0100

    started logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,1 +1,10 @@
+class GameLogic
+    def initialize(player_one, player_two)
+        @player_one = player_one
+        @player_two = player_two
+    end
 
+    def win(board)
+        
+    end
+end
