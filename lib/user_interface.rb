f35517f61ac8cd44d706cc97ad10ae7fcce40f1e
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-16 11:57:21 +0100

    fixed

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,5 +1,6 @@
 class Board
   attr_accessor :board_girds
+
   def initialize
     @board_girds = %w[x x x 4 5 6 7 8 9]
   end
@@ -14,13 +15,13 @@ class Board
   end
 
   def complete_line?(symbol1, symbol2)
-    @board_girds.all? { |cell| cell == symbol1 || cell == symbol2}
+    @board_girds.all? { |cell| cell == symbol1 || cell == symbol2 }
   end
 
   def end_game?
     win_combo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
     win_combo.any? do |combo|
-      board_girds[combo[0]]== board_girds[combo[1]] && board_girds[combo[0]] == board_girds[combo[2]]
+      board_girds[combo[0]] == board_girds[combo[1]] && board_girds[combo[0]] == board_girds[combo[2]]
     end
   end
 end
